1d5e706ffc17e3328776ae60d69edc14e725daae
murodjon <murodjonisroiljonov4@gmail.com>
2025-01-17 18:08:19 +0500

    initial commit

---
+++ js/functioins.js
@@ -0,0 +1,5 @@
+export { validateRegister };
+
+function validateRegister() {
+  return true;
+}
---
+++ js/register.js
@@ -0,0 +1,43 @@
+import { validateRegister } from "./functioins.js";
+
+const form = document.getElementById("registerForm");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form &&
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const isValide = validateRegister();
+    if (!isValide) return;
+
+    const user = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    };
+    fetch("https://auth-rg69.onrender.com/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        if (response.status == 400) {
+          alert("Email yoki username xato");
+        }
+        if (response.status == 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data.message == "User registered successfully!") {
+          location.assign(`${window.location.origin}/pages/login/html`);
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  });
